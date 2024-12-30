77de0bd3cbc1edb2a0577c012d5308641fc673b2
Suryansh-singh-137 <suryanshsingh9736@gmail.com>
2024-12-30 11:46:26 +0000

    container config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,6 @@
+const accountId = 1324
+let accountEmail = "nkr@gmail.com"
+var accountPassword = "123"
+accountCity = "gkp"
+console.table([accountEmail,accountId,accountPassword,accountCity])
+// prrefer noot to use var because of issue in block scope and functional scope
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,15 @@
+"use strict";// it treats the whole code  as neweer version of javascript
+
+// alert (3+3)// it will give error as we are working in node js not in browser
+
+console.log("suryansh");
+
+/* datatypes
+bigint=for large number
+string ="assembly of characters"
+boolean = true or false 
+null = stand alone value (type = object)
+undefined= placeholder for a value which is not defined (type - undefined)
+symbol= uniquenesss */
+
+
