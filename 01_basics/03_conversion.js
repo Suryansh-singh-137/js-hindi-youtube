ce01498e895af310c399000a999fc59f17037879
Suryansh-singh-137 <suryanshsingh9736@gmail.com>
2025-01-08 10:45:53 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,24 @@
+let score = "33abc"
+console.log(typeof(score));
+//console.log(typeof score);
+
+
+let valueNumber = Number(score)
+console.log(typeof valueNumber);
+console.log(valueNumber);
+// "33" => 33 
+// "33abc"= NaN
+// true =  1 , false = 0 ;
+   
+
+ let isLogIn =""
+ let bollIsLogin = Boolean(isLogIn)
+ console.log(bollIsLogin);
+ // 1 =  true 
+ // 0 =  false 
+ // " " = false 
+ // "vdcjs" = true 
+
+
+
+
