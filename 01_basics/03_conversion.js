16bc8e632b440f92e12556970919ea03a9cf21a9
Suryansh-singh-137 <suryanshsingh9736@gmail.com>
2025-01-08 11:06:41 +0000

    operations

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -1,11 +1,11 @@
 let score = "33abc"
-console.log(typeof(score));
+//console.log(typeof(score));
 //console.log(typeof score);
 
 
 let valueNumber = Number(score)
-console.log(typeof valueNumber);
-console.log(valueNumber);
+//console.log(typeof valueNumber);
+//console.log(valueNumber);
 // "33" => 33 
 // "33abc"= NaN
 // true =  1 , false = 0 ;
@@ -13,11 +13,23 @@ console.log(valueNumber);
 
  let isLogIn =""
  let bollIsLogin = Boolean(isLogIn)
- console.log(bollIsLogin);
+ //console.log(bollIsLogin);
  // 1 =  true 
  // 0 =  false 
  // " " = false 
  // "vdcjs" = true 
+
+// ************************** OPERATIOONS***********************
+
+let str1 = "hello"
+let str2  ="world"
+let str3 =  str1 + str2 
+console.log(str3 );//helloworld
+
+console.log("1"+2)// 12
+console.log(1+"2");//12
+console.log("1"+2+3)//123
+console.log(1+2+"3");//33 ----> operation lhs to rhs horha isliye pehle add hogya phir uske bad string mila to jud gya string ban ke
 
 
 
